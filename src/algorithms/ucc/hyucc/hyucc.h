a8de444e523bda38f804f3e71708f8f1ede16093
Michael Polyntsov <Arno9148@gmail.com>
2023-05-08 00:49:39 +0300

    Add threads number option to HyUCC

--- src/algorithms/ucc/hyucc/hyucc.h
+++ src/algorithms/ucc/hyucc/hyucc.h
@@ -5,6 +5,8 @@
 #include "hycommon/types.h"
 #include "model/column_layout_relation_data.h"
 #include "model/idataset_stream.h"
+#include "thread_number/option.h"
+#include "thread_number/type.h"
 #include "ucc/ucc_algorithm.h"
 
 namespace algos {
@@ -12,15 +14,21 @@ namespace algos {
 class HyUCC : public UCCAlgorithm {
 private:
     std::unique_ptr<ColumnLayoutRelationData> relation_;
+    config::ThreadNumType threads_num_ = 1;
 
     void FitInternal(model::IDatasetStream& data_stream) override;
     unsigned long long ExecuteInternal() override;
     void ResetUCCAlgorithmState() override {}
     void RegisterUCCs(std::vector<boost::dynamic_bitset<>>&& uccs,
                       const std::vector<hy::ClusterId>& og_mapping);
+    void MakeExecuteOptsAvailable() final {
+        MakeOptionsAvailable({config::ThreadNumberOpt.GetName()});
+    }
 
 public:
-    HyUCC() : UCCAlgorithm({}) {}
+    HyUCC() : UCCAlgorithm({}) {
+        RegisterOption(config::ThreadNumberOpt(&threads_num_));
+    }
 };
 
 }  // namespace algos
--- src/algorithms/ucc/hyucc/sampler.h
+++ src/algorithms/ucc/hyucc/sampler.h
@@ -2,6 +2,7 @@
 
 #include "hycommon/sampler.h"
 #include "hycommon/types.h"
+#include "options/thread_number/type.h"
 #include "ucc/hyucc/structures/non_ucc_list.h"
 
 namespace algos {
@@ -11,8 +12,8 @@ private:
     hy::Sampler sampler_;
 
 public:
-    Sampler(hy::PLIsPtr plis, hy::RowsPtr pli_records)
-        : sampler_(std::move(plis), std::move(pli_records)) {}
+    Sampler(hy::PLIsPtr plis, hy::RowsPtr pli_records, config::ThreadNumType threads = 1)
+        : sampler_(std::move(plis), std::move(pli_records), threads) {}
 
     NonUCCList GetNonUCCCandidates(hy::IdPairs const& comparison_suggestions) {
         return sampler_.GetAgreeSets(comparison_suggestions);
