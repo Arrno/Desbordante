956cce72b356dcbd92b983ee41613b9b9fd57e2d
Michael Polyntsov <Arno9148@gmail.com>
2024-01-25 00:06:23 +0300

    Avoid redundant lookup in maps in Order where possible

--- src/core/algorithms/order/order.cpp
+++ src/core/algorithms/order/order.cpp
@@ -93,8 +93,8 @@ void Order::CreateSingletonSortedPartitions() {
             }
         }
         equivalence_classes.shrink_to_fit();
-        sorted_partitions_[{i}] =
-                SortedPartition(std::move(equivalence_classes), typed_relation_->GetNumRows());
+        sorted_partitions_.emplace(Node{i}, SortedPartition(std::move(equivalence_classes),
+                                                            typed_relation_->GetNumRows()));
     }
 }
 
@@ -102,11 +102,11 @@ void Order::CreateSortedPartitionsFromSingletons(AttributeList const& attr_list)
     if (sorted_partitions_.find(attr_list) != sorted_partitions_.end()) {
         return;
     }
-    SortedPartition res = sorted_partitions_[{attr_list[0]}];
+    SortedPartition res = sorted_partitions_.at({attr_list[0]});
     for (size_t i = 1; i < attr_list.size(); ++i) {
-        res.Intersect(sorted_partitions_[{attr_list[i]}]);
+        res.Intersect(sorted_partitions_.at({attr_list[i]}));
     }
-    sorted_partitions_[attr_list] = res;
+    sorted_partitions_.emplace(attr_list, res);
 }
 
 void Order::ComputeDependencies(ListLattice::LatticeLevel const& lattice_level) {
